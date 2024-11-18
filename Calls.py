c72c87d29225aaa67551205940790d69664d6348
oneter <113373494+oneter@users.noreply.github.com>
2024-11-18 13:53:29 +0300

    Add files via upload
    Добавлены статические методы валидации полей

--- Calls.py
+++ Calls.py
@@ -40,3 +40,23 @@ class Subscriber:
     def phone(self, value: str):
         self.__validate_phone(value)
         self.__phone = value
+
+    @staticmethod
+    def __validate_name(value: str):
+        if not value or not value.strip():
+            raise ValueError("Имя не может быть пустым.")
+    
+    @staticmethod
+    def __validate_inn(value: str):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("ИНН должен быть строкой из 10 цифр.")
+    
+    @staticmethod
+    def __validate_account(value: str):
+        if not value.isdigit() or len(value) < 20:
+            raise ValueError("Банковский счет должен быть строкой из 20 цифр.")
+    
+    @staticmethod
+    def __validate_phone(value: str):
+        if not value.isdigit() or len(value) < 11:
+            raise ValueError("Телефон должен быть строкой из 11 цифр.")
