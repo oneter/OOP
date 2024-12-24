c0bbc9ed4e358e8bc9d66db41c132a2586cc9139
oneter <113373494+oneter@users.noreply.github.com>
2024-12-24 15:11:16 +0300

    Лаб 2.3 Вынес методы из Subscriber_rep_json в родительский класс

--- Subscriber_rep_json.py
+++ Subscriber_rep_json.py
@@ -15,6 +15,3 @@ class SubscriberRepJson(SubscriberRepository):
         with open(self.file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
         self.save_all(subscribers)
-
-    def get_count(self):
-        return len(self.get_all())
