ef3ef450b477e81eeff4085d2cad16484b9f323e
oneter <113373494+oneter@users.noreply.github.com>
2024-12-26 13:47:00 +0300

    Обновил Subscriber_rep_yaml для реализации стратегии

--- Subscriber_rep_yaml.py
+++ Subscriber_rep_yaml.py
@@ -2,16 +2,22 @@ import yaml
 import os
 from Subscriber import Subscriber
 from SubscriberRepository import SubscriberRepository
+from SubscriberRepFileStrategy import SubscriberRepFileStrategy
 
-class SubscriberRepYaml(SubscriberRepository):
-    """Класс для работы с Subscriber в формате YAML."""
+class SubscriberRepYamlStrategy(SubscriberRepFileStrategy):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
 
-    def _read_file(self):
+    def read(self):
         if not os.path.exists(self.file_path):
             return []
         with open(self.file_path, 'r', encoding='utf-8') as file:
             return yaml.safe_load(file) or []
 
-    def _write_file(self, data):
+    def write(self, data):
         with open(self.file_path, 'w', encoding='utf-8') as file:
             yaml.safe_dump(data, file, allow_unicode=True)
+
+    def display(self):
+        for item in self.read():
+            print(item)
