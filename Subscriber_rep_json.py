ddef07ecaa03f1a7b50c21837581b56392cfd954
oneter <113373494+oneter@users.noreply.github.com>
2024-12-26 13:45:52 +0300

    Обновил Subscriber_rep_json для реализации стратегии

--- Subscriber_rep_json.py
+++ Subscriber_rep_json.py
@@ -2,16 +2,25 @@ import json
 import os
 from Subscriber import Subscriber
 from SubscriberRepository import SubscriberRepository
+import json
+import os
+from SubscriberRepFileStrategy import SubscriberRepFileStrategy
 
-class SubscriberRepJson(SubscriberRepository):
+class SubscriberRepJsonStrategy(SubscriberRepFileStrategy):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
 
-    def _read_file(self):
+    def read(self):
         if not os.path.exists(self.file_path):
             return []
         with open(self.file_path, 'r', encoding='utf-8') as file:
             return json.load(file)
 
-    def _write_file(self, data):
+    def write(self, data):
         with open(self.file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
-        self.save_all(subscribers)
+
+    def display(self):
+        for item in self.read():
+            print(item)
+
